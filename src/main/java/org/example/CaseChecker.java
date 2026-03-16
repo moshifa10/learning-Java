d34f3eb8e420d5b512547e73fc20fd7a2ac34bc1
Njabulo Moshifa <njmosjhb@student.wethinkcode.co.za>
2026-03-16 11:17:41 +0200

    completed name comparison

--- src/main/java/org/example/CaseChecker.java
+++ src/main/java/org/example/CaseChecker.java
@@ -1,4 +1,32 @@
 package org.example;
 
 public class CaseChecker {
+    public static boolean check(String name){
+        // In this function I will be checking for every char and check if they are upper
+
+        for (int i = 0; i < name.length(); i++){
+            char check = name.charAt(i);
+            if (!Character.isUpperCase(check)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+
+        //6. Case Checker
+        //
+        //Write a program that checks if a string is completely uppercase.
+        //If it is, print "All letters are uppercase".
+
+        String name = "NJABS";
+
+
+        if (check(name)){
+            System.out.println("All letters are uppercase");
+        }
+        else {
+            System.out.println("They are not all uppercase");
+        }
+    }
 }
