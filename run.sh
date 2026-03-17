de19df7f20e18578f620a51d54ac4f14acc7d793
Njabulo Moshifa <njmosjhb@student.wethinkcode.co.za>
2026-03-17 14:55:16 +0200

    completed  matrix

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 echo "Njabs aka Majozi"
 echo "I'll be running the script"
 git add .
-git commit -m "added a matrix"
+git commit -m "completed  matrix"
 git push
--- src/main/java/org/example/matrices.java
+++ src/main/java/org/example/matrices.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class matrices {
+
+    public static void main(String[] args){
+
+        int rows ;
+        int column;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        rows = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter number of columns: ");
+        column = scanner.nextInt();
+        for (int i=0; i<=rows; i++){
+            for (int j=0; j<=column; j++){
+                System.out.print("#" + " ");
+            }
+            System.out.println();
+        }
+    }
 }
